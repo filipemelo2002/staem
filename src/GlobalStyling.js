1bc9ba0a12ba74190b7a2876e20210142187f4dd
Filipe Melo <filipe.melo@liferay.com>
2022-06-25 16:56:56 -0300

    Fix global styling

--- src/GlobalStyling.js
+++ src/GlobalStyling.js
@@ -6,6 +6,16 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
+  body {
+    background: rgb(45,66,89);
+    background: radial-gradient(circle, rgba(45,66,89,1) 0%, rgba(27,40,55,1) 100%);
+    font-family: 'Noto Sans', sans-serif;
+    color: white;
+  }
+  
+  button {
+    font-family: 'Noto Sans', sans-serif;
+  }
 `;
 
 
--- src/styles.js
+++ src/styles.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background: rgb(45,66,89);
-  background: radial-gradient(circle, rgba(45,66,89,1) 0%, rgba(27,40,55,1) 100%);
-  font-family: 'Noto Sans', sans-serif;
-  color: white;
   display: grid;
   min-height: 100vh;
+  grid-template-areas: 
+    "header"
+    "carousel"
+    "content"
+  ;
+  grid-template-rows: min-content;
+  max-width: 90rem;
+  margin: 0 auto;
 `;

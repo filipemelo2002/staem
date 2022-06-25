c964f97597a1ad0f34bf5d1f74aa9180f9eef673
Filipe Melo <filipe.melo@liferay.com>
2022-06-25 16:19:06 -0300

    Add initial home styling

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: rgb(45,66,89);
+  background: radial-gradient(circle, rgba(45,66,89,1) 0%, rgba(27,40,55,1) 100%);
+  font-family: 'Noto Sans', sans-serif;
+  color: white;
+  display: grid;
+  min-height: 100vh;
+`;
